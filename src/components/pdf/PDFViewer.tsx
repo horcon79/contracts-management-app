df26d6d041ee69a5140334d24e985e07b7359bac
Horcon79 <horcon.koszalin@gmail.com>
2026-01-04 15:35:32 +0100

    poprawki w wyświetlaniu PDF w oknie aplikacji  - nowsza wersja pdf.js wymaga ładowania workera jako modułu ESM

--- src/components/pdf/PDFViewer.tsx
+++ src/components/pdf/PDFViewer.tsx
@@ -8,7 +8,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 
 // Configure pdf.js worker
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 interface PDFViewerProps {
     file: string;
